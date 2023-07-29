0956f0e9b0ba83bb61a3311969f8c287d3daab1f
ophx <castytastyhappybat@gmail.com>
2023-07-28 22:22:34 -0400

    todo: animation

--- src/pages/panel/Settings.tsx
+++ src/pages/panel/Settings.tsx
@@ -19,6 +19,8 @@ export const Settings = () => {
         }
     }
 
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
     return (
         <div className="flex">
             <Sidebar />
@@ -29,19 +31,19 @@ export const Settings = () => {
                 <div className="border-b border-[#222]"></div>
 
                 <div className="grid grid-cols-2 gap-2">
-                    <div className={`p-2 border ${checked ? "border-purple-500/75" : "border-[#B5290B]/75"}`}>
+                    <div className={`p-2 border ${checked ? `border-[#${randomColor}]` : "border-[#B5290B]/75"}`}>
                         <p className="text-base text-white mb-1">website</p>
                         <div className="flex items-center gap-2">
-                            <input checked={checked} type="checkbox" name="gamesense_mode" id="gamesense_mode" onChange={handleGsCheck} className={`peer relative appearance-none shrink-0 w-4 h-4 border-2 border-transparent ${checked ? "checked:border-purple-500 checked:bg-purple-500" : "checked:border-[#B5290B] checked:bg-[#B5290B]"} rounded-sm mt-1 bg-white outline-none ring-none checked:border-0`} />
+                            <input checked={checked} type="checkbox" name="gamesense_mode" id="gamesense_mode" onChange={handleGsCheck} className={`peer relative appearance-none shrink-0 w-4 h-4 border-2 border-transparent ${checked ? `checked:border-[#${randomColor}] checked:bg-[#${randomColor}]` : "checked:border-[#B5290B] checked:bg-[#B5290B]"} rounded-sm mt-1 bg-white outline-none ring-none checked:border-0`} />
                             <label htmlFor="gamesense_mode" className="text-white select-none">gamesense mode (rainbow theme)</label>
                         </div>
                     </div>
-                    <div className={`p-2 border ${checked ? "border-purple-500/75" : "border-[#B5290B]/75"}`}>
+                    <div className={`p-2 border ${checked ? `border-[#${randomColor}]` : "border-[#B5290B]/75"}`}>
                         <p className="text-base text-white mb-1">discord</p>
-                        <button type="submit" className={`px-2.5 ring-none outline-none border ${checked ? "border-purple-500/75" : "border-[#B5290B]/75"} text-white placeholder-gray-400 bg-[#000]`}>link account</button>
-                        <p className={`text-sm ${checked ? "text-purple-500" : "text-[#B5290B]"} mt-1`}>currently linked: @Leonoell</p>
+                        <button type="submit" className={`px-2.5 ring-none outline-none border ${checked ? `border-[#${randomColor}]` : "border-[#B5290B]/75"} text-white placeholder-gray-400 bg-[#000]`}>link account</button>
+                        <p className={`text-sm ${checked ? `text-[#${randomColor}]` : "text-[#B5290B]"} mt-1`}>currently linked: @Leonoell</p>
                     </div>
-                    <div className={`p-2 border ${checked ? "border-purple-500/75" : "border-[#B5290B]/75"}`}>
+                    <div className={`p-2 border ${checked ? `border-[#${randomColor}]` : "border-[#B5290B]/75"}`}>
                         <p className="text-base text-white mb-1">active subs</p>
                         <p className="text-sm text-green-500">roblox - v1.0.0 (420 days left)</p>
                     </div>
