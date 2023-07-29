e113191e705368c2676dcc25504c2059fe06a772
ophx <castytastyhappybat@gmail.com>
2023-07-29 15:35:48 -0400

    border on footer. maybe not keeping idk yet

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,15 @@
+import { useGlobalState } from "../states/globalState";
+
 export const Footer = () => {
+    const [ checked ] = useGlobalState("gsModeChecked");
+    
+    const [ randomColor ] = useGlobalState("randomColor");
+
     return (
         <div className="fixed bottom-4">
-            <p className="text-gray-400 text-center text-sm">website made with 💖 by ophx</p>
+            <div className={`p-2 border ${checked ? `border-[${randomColor}]` : "border-[#B5290B]/75"}`}>
+                <p className="text-gray-400 text-center text-sm">website made with 💖 by ophx</p>
+            </div>
         </div>
     )
 }
