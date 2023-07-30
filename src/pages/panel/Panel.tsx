95dcf1f2b55aa48a228b3b0d3cd4e92f58ab9abd
ophx <castytastyhappybat@gmail.com>
2023-07-29 23:28:39 -0400

    m

--- src/pages/panel/Panel.tsx
+++ src/pages/panel/Panel.tsx
@@ -26,7 +26,7 @@ export const Panel = () => {
                         <div className="grid grid-cols-3 gap-2">
                             <div className={`p-2 border ${checked ? `border-[${randomColor}]` : "border-[#B5290B]/75"}`}>
                                 <p className="text-base text-white">status</p>
-                                <p className="text-gray-400"><span className="text-yellow-500">in development</span></p>
+                                <p className="text-gray-400"><span className="text-purple-500">in development</span></p>
                             </div>
                             <div className={`p-2 border ${checked ? `border-[${randomColor}]` : "border-[#B5290B]/75"}`}>
                                 <p className="text-base text-white">users</p>
@@ -38,10 +38,24 @@ export const Panel = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="w-1/2">
+                    <div className="w-1/2 space-y-2">
+                        <div className={`p-2 border ${checked ? `border-[${randomColor}]` : "border-[#B5290B]/75"}`}>
+                            <p className="text-base text-white mb-1">quick actions</p>
+                            <div className="flex flex-col gap-2">
+                                <button type="submit" className={`px-2.5 ring-none outline-none border ${checked ? `border-[${randomColor}]` : "border-[#B5290B]/75"} text-white placeholder-gray-400 bg-[#000]`}>
+                                    rules
+                                </button>
+                                <button type="submit" className={`px-2.5 ring-none outline-none border ${checked ? `border-[${randomColor}]` : "border-[#B5290B]/75"} text-white placeholder-gray-400 bg-[#000]`}>
+                                    tos
+                                </button>
+                                <button type="submit" className={`px-2.5 ring-none outline-none border ${checked ? `border-[${randomColor}]` : "border-[#B5290B]/75"} text-white placeholder-gray-400 bg-[#000]`}>
+                                    join our discord server
+                                </button>
+                            </div>
+                        </div>
                         <div className={`p-2 border ${checked ? `border-[${randomColor}]` : "border-[#B5290B]/75"}`}>
                             <div>
-                                <iframe width="100%" height="300" src="https://www.youtube.com/embed/AX8Xm6maEYw" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                                <iframe width="100%" height="300" src="https://www.youtube.com/embed/05lBjTboLrs" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                             </div>
                         </div>
                     </div>
